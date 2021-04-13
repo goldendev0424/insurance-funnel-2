28a16150c3e8afe887ca49566092c6035015fe3e
goldendev0424 <golden70044@gmail.com>
2021-04-14 06:55:01 +1000

    Fix progressbar color

--- components/ProgressBar.vue
+++ components/ProgressBar.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div class="w-full h-3 rounded-full overflow-hidden bg-gray-300">
     <div
-      class="bg-pink-600 h-full rounded-full transition-all duration-300"
+      class="bg-green-500 h-full rounded-full transition-all duration-300"
       :style="{ width: `${((current + 1) / (steps + 1)) * 100}%` }"
     ></div>
   </div>
