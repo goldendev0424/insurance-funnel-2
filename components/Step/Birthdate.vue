0778ccb4cf1b9f115adf163a16aa6f2ce9cce723
goldendev0424 <golden70044@gmail.com>
2021-04-14 09:18:01 +1000

    Fix validate birthday

--- components/Step/Birthdate.vue
+++ components/Step/Birthdate.vue
@@ -2,21 +2,26 @@
   <ValidationObserver ref="observer" class="w-full mt-6" tag="div">
     <Title>Birthdate</Title>
     <div class="flex flex-col items-center">
-      <TextInput
-        v-model="birthdate"
-        class="mb-5"
-        mask="##  /  ##  /  ####"
-        rules="birthdate"
-        :errors="errors"
-      ></TextInput>
-
+      <div class="mb-6">
+        <div class="flex p-5 text-xl text-blue-700 font-bold shadow rounded duration-500 border focus:border-blue-500 bg-white">
+          <input class="outline-none text-center text-black focus:text-blue-700" size="2" maxLength="2" placeholder="MM"
+            v-model="birthdate.month" ref="monthInput" />
+          <span class="mx-3">/</span>
+          <input class="outline-none text-center text-black focus:text-blue-700" size="2" maxLength="2" placeholder="DD"
+            v-model="birthdate.date" ref="dateInput" />
+          <span class="mx-3">/</span>
+          <input class="outline-none text-center text-black focus:text-blue-700" size="4" maxLength="4" placeholder="YYYY"
+            v-model="birthdate.year" ref="yearInput" />
+        </div>
+        <ValidationErrorText> {{ error }} </ValidationErrorText>
+      </div>
       <PrimaryButton @click="next" class="w-1/2">CONTINUE</PrimaryButton>
     </div>
   </ValidationObserver>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Vue, Watch } from 'nuxt-property-decorator'
 
 import Title from '~/components/Title.vue'
 import TextInput from '~/components/TextInput.vue'
@@ -33,29 +38,142 @@ import ValidationErrorText from '~/components/ValidationErrorText.vue'
 })
 export default class Birthdate extends Vue {
   $refs!: {
-    observer: Vue & {
-      validateWithInfo: () => Promise<{ isValid: boolean; errors: string[] }>
+    dateInput: Vue & {
+      focus: () => {}
+    }
+    yearInput: Vue & {
+      focus: () => {}
     }
   }
 
-  errors: string[] = []
-
-  birthdate = ''
-
-  created() {
-    this.birthdate = this.$store.state.birthdate
+  error: string = ''
+  birthdate = {
+    year: '',
+    month: '',
+    date: '',
   }
 
-  async next() {
-    this.errors = []
-    const res = await this.$refs.observer.validateWithInfo()
+  created() {
+    let birthdateStr = this.$store.state.birthdate
+    let date = birthdateStr.split('/')
+    let cleanDate = date.map((item: string) => {
+      return item.trim()
+    })
 
-    if (!res.isValid) {
-      this.errors = Object.values(res.errors).flat()
+    this.birthdate = {
+      year: cleanDate[2],
+      month: cleanDate[0],
+      date: cleanDate[1],
+    }
+  }
+
+  @Watch('birthdate.year')
+  onYearChanged(value: any, oldValue: string) {
+    // If year is not a number
+    if (isNaN(value)) {
+      this.birthdate.year = oldValue
+    }
+  }
+
+  @Watch('birthdate.month')
+  onMonthChanged(value: any, oldValue: string) {
+    // If month is not a number
+    if (isNaN(value)) {
+      this.birthdate.month = oldValue
+    }
+
+    if (this.birthdate.month.length < 2) {
       return
     }
 
-    this.$store.commit('SET_BIRTHDATE', this.birthdate)
+    // If month inputed
+    // validate month
+    if (value < 1) {
+      this.birthdate.month = '01'
+    }
+
+    if (value > 12) {
+      this.birthdate.month = '12'
+    }
+    this.$refs.dateInput.focus()
+  }
+
+  @Watch('birthdate.date')
+  onDateChanged(value: any, oldValue: string) {
+    // If date is not a number
+    if (isNaN(value)) {
+      this.birthdate.date = oldValue
+    }
+
+    if (this.birthdate.date.length < 2) {
+      return
+    }
+
+    // If date inputed
+    // Validate date
+    if (value < 1) {
+      this.birthdate.date = '01'
+    }
+
+    let month = Number(this.birthdate.month)
+    month = month < 1 ? (month = 1) : month
+
+    let dt = new Date(Number(this.birthdate.year), month, 1)
+
+    if (value > 31) {
+      this.birthdate.date = this.endOfMonth(dt).toString()
+    }
+
+    this.$refs.yearInput.focus()
+  }
+
+  // Get end date of month
+  endOfMonth(date: Date) {
+    return new Date(date.getFullYear(), date.getMonth(), 0).getDate()
+  }
+
+  addPrefixDate(d: number) {
+    return d < 10 ? `0${d}` : d
+  }
+
+  async next() {
+    let dt = new Date(
+      Number(this.birthdate.year),
+      Number(this.birthdate.month) - 1,
+      Number(this.birthdate.date)
+    )
+    this.error = ''
+
+    if (dt.toString().toLowerCase() === 'invalid date') {
+      this.error = 'Please enter a valid date'
+      return
+    }
+
+    let year = dt.getFullYear().toString()
+    let month = this.addPrefixDate(dt.getMonth() + 1).toString()
+    let date = this.addPrefixDate(dt.getDate()).toString()
+
+    this.birthdate = {
+      year,
+      month,
+      date,
+    }
+
+    // Validate age
+    if (Number(year) < 1950) {
+      this.error = 'Please enter a valid date'
+      return
+    }
+
+    if (new Date().getFullYear() < Number(year) + 18) {
+      this.error = 'Driver must be at least 18 years old'
+      return
+    }
+
+    // Store birthday and go next step
+    let birthdateStr = `${month}/${date}/${year}`
+    console.log(birthdateStr)
+    this.$store.commit('SET_BIRTHDATE', birthdateStr)
     this.$emit('next')
   }
 }
--- components/TextInput.vue
+++ components/TextInput.vue
@@ -1,18 +1,5 @@
 <template lang="html">
   <ValidationProvider slim="slim" :rules="rules" tag="div">
-    <!-- <input
-      v-if="mask"
-      v-mask="mask"
-      class="p-5 leading-none tracking-wide text-xl border rounded-sm bg-gray-200 text-blue-700 font-semibold border-gray-400 placeholder-gray-500 placeholder-normal placeholder focus:font-normal focus:bg-white sm:block sm:w-full sm:p-3 sm:text-base sm:text-center"
-      v-bind="$attrs"
-      @input="$emit('input', $event.target.value)"
-    />
-    <input
-      v-else
-      class="p-5 leading-none tracking-wide text-xl border rounded-sm bg-gray-200 text-blue-700 font-semibold border-gray-400 placeholder-gray-500 placeholder-normal placeholder focus:font-normal focus:bg-white sm:block sm:w-full sm:p-3 sm:text-base sm:text-center"
-      v-bind="$attrs"
-      @input="$emit('input', $event.target.value)"
-    /> -->
     <input
       class="p-5 sm:p-3 leading-none tracking-wide text-xl text-blue-700 shadow rounded duration-500 border outline-none hover:border-blue-500 focus:border-blue-500
           bg-gray-200 font-bold placeholder-normal focus:bg-white sm:block w-full sm:text-base sm:text-center"
