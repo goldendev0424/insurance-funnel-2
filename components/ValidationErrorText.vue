6ff9a79de758539937f8689d35fe1c7dcfb8347e
goldendev0424 <golden70044@gmail.com>
2021-04-14 23:30:23 +1000

    Fix validation error text

--- components/ValidationErrorText.vue
+++ components/ValidationErrorText.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div
     class="text-base mmd:text-lg text-white mmd:text-red-600 border-0 mmd:border-l-4 border-red-600 pl-0 mmd:pl-2 text-center mmd:text-left bg-red-600 mmd:bg-white
-          font-semibold mmd:font-bold mt-3 mmd:mt-4"
+          font-semibold mmd:font-bold mt-3 px-2"
   >
     <slot></slot>
   </div>
